784f6511e8dbba310aef7260d1857b5e9463311a
Lorenzo <henriquewillians@usp.br>
2024-08-22 19:47:53 -0300

    carousel improvisado

--- frontend/easytickets/src/app/page.tsx
+++ frontend/easytickets/src/app/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar.'; // Ensure correct path and name
+import Carousel from './components/Carousel';
 
-const OPTIONS = { loop: true };
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
       <main style={styles.mainContent as React.CSSProperties}>
-        <h1>carrosel</h1>
+        <Carousel/>
       </main>
     </div>
   );
