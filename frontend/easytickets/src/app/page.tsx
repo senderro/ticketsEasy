0eec993104c437f43a6be8544419c9c43f7acb18
Lorenzo <henriquewillians@usp.br>
2024-08-22 19:37:49 -0300

    retiradaerro

--- frontend/easytickets/src/app/page.tsx
+++ frontend/easytickets/src/app/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar.'; // Ensure correct path and name
-import EmblaCarousel from './components/EmblaCarrossel';
-import EmblaOptionsType from "embla-carousel-react"
 
 const OPTIONS = { loop: true };
 const SLIDE_COUNT = 5;
@@ -12,7 +10,7 @@ const Home: React.FC = () => {
     <div>
       <Navbar />
       <main style={styles.mainContent as React.CSSProperties}>
-        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+        <h1>carrosel</h1>
       </main>
     </div>
   );
